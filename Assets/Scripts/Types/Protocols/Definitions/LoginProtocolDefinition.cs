21e20fa07a3efbf67d50f610229c9e55d823e476
Luis Masuelli <luis@datagran.io>
2022-05-14 12:36:12 -0500

    Slight change on Login message definition.

--- Assets/Scripts/Types/Protocols/Definitions/LoginProtocolDefinition.cs
+++ Assets/Scripts/Types/Protocols/Definitions/LoginProtocolDefinition.cs
@@ -3,18 +3,21 @@ using AlephVault.Unity.Meetgard.Auth.Protocols.Simple;
 using AlephVault.Unity.Meetgard.Types;
 using GGJUIO2020.Types.Protocols.Messages;
 
+
 namespace GGJUIO2020.Types
 {
     namespace Protocols
     {
         namespace Definitions
         {
+            /// <summary>
+            ///   A login protocol with unsecured User/Pass.
+            /// </summary>
             public class SampleLoginProtocolDefinition : SimpleAuthProtocolDefinition<Nothing, LoginFailed, Kicked>
             {
                 protected override void DefineLoginMessages()
                 {
-                    DefineLoginMessage<LoginBody>("login:community");
-                    throw new System.NotImplementedException();
+                    DefineLoginMessage<LoginBody>("Login:Community");
                 }
             }
         }
