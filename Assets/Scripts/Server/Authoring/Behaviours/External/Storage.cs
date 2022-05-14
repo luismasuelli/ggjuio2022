1efce8f5182310e834cbe86055ff198fda7d1962
Luis Masuelli <luis@datagran.io>
2022-05-14 11:40:11 -0500

    Adding the remote storage (initial class - a sample is still needed).

---
+++ Assets/Scripts/Server/Authoring/Behaviours/External/Storage.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using AlephVault.Unity.RemoteStorage.StandardHttp.Types;
+using AlephVault.Unity.Support.Generic.Authoring.Types;
+using AlephVault.Unity.RemoteStorage.Types.Results;
+using GGJUIO2020.Server.Types.RemoteStorage;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace GGJUIO2020.Server
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace External
+            {
+                public class Storage : MonoBehaviour
+                {
+                    public class UserException : Exception
+                    {
+                        /// <summary>
+                        ///   The error code.
+                        /// </summary>
+                        public readonly ResultCode ErrorCode;
+
+                        public UserException(ResultCode code) : base("Resource error")
+                        {
+                            ErrorCode = code;
+                        }
+                    }
+                    
+                    /// <summary>
+                    ///   The API key.
+                    /// </summary>
+                    [SerializeField]
+                    private string ApiKey;
+
+                    // The root handler.
+                    private Root httpRoot;
+
+                    // The users handler.
+                    private ListResource<User, User> users;
+
+                    private void Awake()
+                    {
+                        httpRoot = new Root("http://localhost:6666", new Authorization("Bearer", "sample-abcdef"));
+                        users = (ListResource<User, User>)httpRoot.GetList<User, User>("players");
+                    }
+
+                    /// <summary>
+                    ///   Gets a user by its login account.
+                    /// </summary>
+                    /// <param name="login">The login</param>
+                    /// <exception cref="Exception">When something is not ok, or missing</exception>
+                    public async Task<User> GetByLogin(string login)
+                    {
+                        Result<JObject, string> result = await users.View(
+                            "by-login", new Dictionary<string, string>() {{"login", login}}
+                        );
+                        if (result.Code == ResultCode.Ok)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return serializer.Deserialize<User>(new JTokenReader(result.Element));
+                        }
+                        else if (result.Code == ResultCode.DoesNotExist)
+                        {
+                            throw new UserException(ResultCode.DoesNotExist);
+                        }
+                        else
+                        {
+                            Debug.LogError($"HTTP Error code is: {result.Code}");
+                            throw new UserException(ResultCode.ServerError);
+                        }
+                    }
+
+                    /// <summary>
+                    ///   Registers a user by its full data (_id is ignored).
+                    /// </summary>
+                    /// <param name="user">The whole user data</param>
+                    public async Task Create(User user)
+                    {
+                        Result<User, string> result = await users.Create(user);
+                        if (result.Code == ResultCode.Created)
+                        {
+                            // Everything ok.
+                            return;
+                        }
+                        else if (result.Code == ResultCode.DuplicateKey)
+                        {
+                            throw new UserException(ResultCode.DuplicateKey);
+                        }
+                        else if (result.Code == ResultCode.ValidationError)
+                        {
+                            throw new UserException(ResultCode.ValidationError);
+                        }
+                        else
+                        {
+                            Debug.LogError($"HTTP Error code is: {result.Code}");
+                            throw new UserException(ResultCode.ServerError);
+                        }
+                    }
+
+                    /// <summary>
+                    ///   Updates (i.e. fully replaces) a user record.
+                    /// </summary>
+                    /// <param name="user">The user to update. The _id and its data are taken into account</param>
+                    public async Task Update(User user)
+                    {
+                        Result<User, string> result = await users.Replace(user.Id, user);
+                        if (result.Code == ResultCode.Created)
+                        {
+                            // Everything ok.
+                            return;
+                        }
+                        else if (result.Code == ResultCode.DuplicateKey)
+                        {
+                            throw new UserException(ResultCode.DuplicateKey);
+                        }
+                        else if (result.Code == ResultCode.ValidationError)
+                        {
+                            throw new UserException(ResultCode.ValidationError);
+                        }
+                        else
+                        {
+                            Debug.LogError($"HTTP Error code is: {result.Code}");
+                            throw new UserException(ResultCode.ServerError);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
