e0bad2260261636cdb549a09b9b301e2e7c6a38a
Luis Masuelli <luis@datagran.io>
2022-05-15 16:53:13 -0500

    Adding informant and referee behaviours.

---
+++ Assets/Scripts/Client/Authoring/Behaviours/Protocols/Models/InformantClientSide.cs
@@ -0,0 +1,34 @@
+using AlephVault.Unity.Meetgard.Types;
+using GameMeanMachine.Unity.NetRose.Authoring.Behaviours.Client;
+using GGJUIO2020.Types.Protocols.Messages;
+using TMPro;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+using Vector3 = AlephVault.Unity.Binary.Wrappers.Vector3;
+
+
+namespace GGJUIO2020.Client
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Protocols
+            {
+                namespace Models
+                {
+                    public class InformantClientSide : NetRoseModelClientSide<Nothing, Nothing>
+                    {
+                        protected override void InflateFrom(Nothing fullData)
+                        {
+                        }
+
+                        protected override void UpdateFrom(Nothing refreshData)
+                        {
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
