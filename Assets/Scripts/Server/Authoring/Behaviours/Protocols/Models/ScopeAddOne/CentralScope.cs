3dfbc9cbb587642828e9a8ac05bccfd84f015610
Luis Masuelli <luis@datagran.io>
2022-05-14 19:19:35 -0500

    Adding the behaviour for central scope. Depends on a "referee" prefab existing.

---
+++ Assets/Scripts/Server/Authoring/Behaviours/Protocols/Models/ScopeAddOne/CentralScope.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AlephVault.Unity.Meetgard.Scopes.Authoring.Behaviours.Server;
+using GameMeanMachine.Unity.NetRose.Authoring.Behaviours.Server;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.World;
+using UnityEngine;
+
+
+namespace GGJUIO2020.Server
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Protocols
+            {
+                namespace Models
+                {
+                    namespace ScopeAddOns
+                    {
+                        [RequireComponent(typeof(NetRoseScopeServerSide))]
+                        public class CentralScope : MonoBehaviour
+                        {
+                            private NetRoseScopeServerSide netRoseScopeServerSide;
+                            
+                            private void Awake()
+                            {
+                                netRoseScopeServerSide = GetComponent<NetRoseScopeServerSide>();
+                                netRoseScopeServerSide.ScopeServerSide.OnLoad += async () =>
+                                {
+                                    Map map = GetComponentInChildren<Map>();
+                                    ushort REFEREE_X = 7;
+                                    ushort REFEREE_Y = 3;
+
+                                    try
+                                    {
+                                        var obj = netRoseScopeServerSide.NetRoseProtocolServerSide.InstantiateHere(
+                                            "referee", async (obj) =>
+                                            {
+                                                MapObject mapObj = obj.GetComponent<MapObject>();
+                                                mapObj.Attach(map, REFEREE_X, REFEREE_Y, true);
+                                            }
+                                        );
+                                    }
+                                    catch (KeyNotFoundException e)
+                                    {
+                                        Console.WriteLine("Wrong prefab index! Invalid for referee");
+                                        throw;
+                                    }
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
